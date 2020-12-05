38cdea22ee70bdc9112a71beaa373bee48c3f4fd
Bence <16864830+jozgyb@users.noreply.github.com>
2020-12-05 11:03:44 +0100

    adatbázis konfiguráció hozzáadása

---
+++ includes/db_config.inc.php
@@ -0,0 +1,15 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'csodacs');
+define('DB_PASSWORD', 'aP6dR9w17S9rv9PL');
+define('DB_NAME', 'csodacsoport');
+ 
+/* Attempt to connect to MySQL database */
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}

3671e4ab6cb6be5f957461462227041e62903ab0
Bence <16864830+jozgyb@users.noreply.github.com>
2020-12-06 16:03:35 +0100

    dinamikus galléria képfeltöltéssel

--- templates/pages/gallery.tpl.php
+++ templates/pages/gallery.tpl.php
@@ -1,34 +1,115 @@
-<div class="gallery">
+<?php
+$target_dir = "img/uploads/";
+$uploadOk = 1;
 
-    <a href="img/voulenteers.jpg" rel="rel1">
-        <img src="img/voulenteers.jpg" alt="" title="Image 1">
-    </a>
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    <a href="img/voulenteers2.jpg" rel="rel1">
-        <img src="img/voulenteers2.jpg" alt="" title="Image 2">
-    </a>
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "A fájl kép- " . $check["mime"] . ". \n";
+        $uploadOk = 1;
+    } else {
+        echo "A kiválasztott fájl nem egy kép.\n";
+        $uploadOk = 0;
+    }
 
-    <a href="img/donation1.jpg" rel="rel1">
-        <img src="img/donation1.jpg" alt="" title="Image 3">
-    </a>
 
-    <a href="img/team2.png" rel="rel1">
-        <img src="img/team2.png" alt="" title="Image 4">
-    </a>
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sajnos már létezik kép ezzel a névvel.";
+        $uploadOk = 0;
+    }
 
-    <script>
-        var gallery = $('.gallery a').simpleLightbox({
-            /* options */
-        });
-    </script>
+    // Check file size
+    elseif ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sajnos a fájl mérete túl nagy.";
+        $uploadOk = 0;
+    }
 
-    <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            Select image to upload:
-            <input type="file" name="fileToUpload" id="fileToUpload">
-            <input type="submit" value="Upload Image" name="submit">
-        </form>
-    <?php } ?>
+    // Allow certain file formats
+    elseif (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        echo "Csak JPG, JPEG, PNG & GIF kiterjesztésű képek feltöltése engedélyezett.";
+        $uploadOk = 0;
+    }
 
+    // Check if $uploadOk is set to 0 by an error
+    else if ($uploadOk == 0) {
+        echo "Hiba történt.";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "A következő fájl: " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " feltöltésre került";
+        } else {
+            echo "Hiba a felöltés közben.";
+        }
+    }
+}
+?>
+
+<?php
+$images = array();
+$galleryDir = "img/uploads";
+if ($handle = opendir($galleryDir)) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            $images[filemtime($galleryDir . "/" . $entry)] = $entry;
+        }
+    }
+
+    closedir($handle);
+}
+ksort($images);
+$iter = 0;
+foreach ($images as $image) {
+    if ($iter % 3 == 0) {
+        echo "<div class=\"row\">";
+    }
+    $imgSource = $galleryDir . "/" . $image;
+    echo "<div class=\"col-md-4\">\n <div class=\"thumbnail\">\n";
+    echo "<a href=\"" . $imgSource . "\">\n";
+    echo "<img src=\"" . $imgSource . "\" alt=\"\" style=\"width:100%\">\n";
+    echo "</a>\n";
+    echo "</div>\n </div>\n";
+    if ($iter % 3 == 2) {
+        echo "</div>\n";
+    }
+    $iter++;
+}
+?>
+
+<script>
+    var gallery = $('.thumbnail a').simpleLightbox({
+        /* options */
+    });
+</script>
+
+<?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <form method="post" enctype="multipart/form-data">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Válassza ki a feltölteni kívánt képet:</span>
+                        </div>
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" name="fileToUpload" id="fileToUpload">
+                            <label class="custom-file-label" for="fileToUpload">Fájl kiválasztása</label>
+                        </div>
+                        <div class="input-group-append">
+                            <input type="submit" class="input-group-text" value="Kép feltöltése" name="submit">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php } ?>
 
 </div>

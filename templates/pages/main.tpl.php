f45e41d242fea30ad1e9729c470bfa47a684e962
Bence <16864830+jozgyb@users.noreply.github.com>
2020-12-06 15:22:08 +0100

    főoldal design frissítés

--- templates/pages/main.tpl.php
+++ templates/pages/main.tpl.php
@@ -1,11 +1,13 @@
 <figure>
-    <div class="fixed-wrap">
-        <div id="fixed">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <img src="img/welcome.png" class="img-fluid" alt="Responsive image">
+            </div>
         </div>
     </div>
 </figure>
-<div class="containter fluid padding">
+<div class="container-fluid padding">
     <div class="row welcome text-center">
         <div class="col-12">
             <h2 class="display-5">
@@ -24,6 +26,22 @@
                     Csatlakozom
                 </button>
             </a>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="mx-auto col-md-6">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AroBoMNeYrA"></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="mx-auto col-md-6">
+            <video class="embed-responsive embed-responsive-16by9" controls>
+                <source src="vid/stock.mp4" type="video/mp4">
+            </video>
         </div>
     </div>
 </div>
